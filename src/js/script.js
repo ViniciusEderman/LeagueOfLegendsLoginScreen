2c0a7a65b03a9334743c2d8c58e77a06c8ea2109
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-06-24 20:06:53 -0300

    Bug no removeAttribute

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 
 const input = document.querySelectorAll('.input');
+const button = document.querySelectorAll('.login_button');
 
 const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
@@ -13,9 +14,20 @@ const handleFocusOut = ({ target }) => {
     }
 }
 
+const handleChange = () => {
+    const [userName, password] = input;
+    /* explicando a linha 17: seria a mesma coisa que fazer:
+        const userName = input[0];
+        const password = input[1];
+    */
+    if(userName.value && password.value.length >= 8) {
+        button.removeAttribute('disabled');
+    }
+    else {
+        button.setAttribute('disabled', '');
+    }
+}
+
 input.forEach((input) => input.addEventListener('focus', handleFocus));
 input.forEach((input) => input.addEventListener('focusOut', handleFocusOut));
-
-// este código js não está funcionando
-// entender o que está acontecendo
-// resolver e testar para continuar com a estilização 
+input.forEach((input) => input.addEventListener('input', handleChange));
